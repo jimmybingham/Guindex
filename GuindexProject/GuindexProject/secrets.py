d6173f404579754924ad9b20d44cb5a9005abd8c
howardrj <howardrj@tcd.ie>
2017-12-14 09:46:24 +0000

    First commit of guindex. First pass of backend mostly completed.

---
+++ GuindexProject/GuindexProject/secrets.py
@@ -0,0 +1,4 @@
+EMAIL = "everythingoftheory10@gmail.com"
+PASSWORD = "28beechdale"
+KEY = '0xgsh71z@l0hlzd3qdaj-&3q)&ujwslaj8dhcju_0u+sj66w6c'
+BOT_HTTP_API_TOKEN = '197671869:AAH8dNkBfoz1CqQEIjXAvGt2HN2YA2QxfUw'

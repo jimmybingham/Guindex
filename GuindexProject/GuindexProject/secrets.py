b9a74e8a91498045f40a5b8103c80a6bf82ad27f
howardrj <howardrj@tcd.ie>
2017-12-29 14:49:52 +0000

    Removed secrets.py.

--- GuindexProject/GuindexProject/secrets.py
+++
@@ -1,4 +0,0 @@
-EMAIL = "everythingoftheory10@gmail.com"
-PASSWORD = "28beechdale"
-KEY = '0xgsh71z@l0hlzd3qdaj-&3q)&ujwslaj8dhcju_0u+sj66w6c'
-BOT_HTTP_API_TOKEN = '537855299:AAHYYmw_1-T8dsxiHmQXXFrCTPAXiP5JNVg'
